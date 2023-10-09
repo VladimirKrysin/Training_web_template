c42ab02a5f2b49fb36680bbe3cb8cd465c209c51
VladimirKrysin <140489514+VladimirKrysin@users.noreply.github.com>
2023-10-09 12:34:34 +0300

    Add files via upload
    Finished responsive layout

--- main.js
+++ main.js
@@ -5,5 +5,15 @@ $(function(){
     variableWidth: true,
     prevArrow: '<img class="arrow arrow-left" src="images/arrow-left.svg" alt=""></img>',
     nextArrow: '<img class=" arrow arrow-right" src="images/arrow-right.svg" alt="">',
+    responsive: [
+      {
+        breakpoint: 361,
+        settings: {
+          variableWidth: false,
+          slidesToShow: 1
+        }
+
+      }
+    ]
   });
 });
